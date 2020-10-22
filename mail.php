d39592f2c009339cf021c4b857b27ed104f179e5
wille <willejonsson80@gmail.com>
2020-10-23 00:12:30 +0200

    sdd

--- mail.php
+++ mail.php
@@ -1,16 +1,15 @@
 <?php
-if(!empty($_POST["send"])) {
-	$name = $_POST["userName"];
-	$email = $_POST["userEmail"];
-	$subject = $_POST["subject"];
-	$content = $_POST["content"];
+if(isset( $_POST['name']))
+$name = $_POST['name'];
+if(isset( $_POST['email']))
+$email = $_POST['email'];
+$subject = $_POST["subject"];
+$content = $_POST["content"];
 
-	$toEmail = "willejonsson80@gmail.com";
-	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
-	if(mail($toEmail, $subject, $content, $mailHeaders)) {
-	    $message = "Your contact information is received successfully.";
-	    $type = "success";
-	}
-}
-require_once "contact-view.php";
+
+$content="From:";
+$recipient = "willejonsson80@gmail.com";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $content, $mailheader) or die("Error!");
+echo "Email sent!";
 ?>

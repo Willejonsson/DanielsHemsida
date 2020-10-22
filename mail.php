3246b5f02c685e5f08d1bf28bb8471e9ff5caaa3
wille <willejonsson80@gmail.com>
2020-10-22 23:55:29 +0200

    gg

--- mail.php
+++ mail.php
@@ -7,7 +7,7 @@ $subject = $_POST["subject"];
 $content = $_POST["content"];
 
 
-$content="From: $name";
+
 $recipient = "willejonsson80@gmail.com";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $content, $mailheader) or die("Error!");

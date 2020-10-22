0a2586f34377a6b5bb94b4ce797cae4a6f9f7df8
wille <willejonsson80@gmail.com>
2020-10-22 23:51:10 +0200

    test mail

--- mail.php
+++ mail.php
@@ -3,12 +3,13 @@ if(isset( $_POST['name']))
 $name = $_POST['name'];
 if(isset( $_POST['email']))
 $email = $_POST['email'];
-
+$subject = $_POST["subject"];
+$content = $_POST["content"];
 
 
 $content="From: $name";
 $recipient = "willejonsson80@gmail.com";
 $mailheader = "From: $email \r\n";
-mail($recipient, $subject, print $content, print $mailheader) or die("Error!");
+mail($recipient, $subject, $content, $mailheader) or die("Error!");
 echo "Email sent!";
 ?>

cb8e25268df482a31926816e168f96cbb39c711f
wille <willejonsson80@gmail.com>
2020-10-23 09:35:30 +0200

    dd

--- mail.php
+++ mail.php
@@ -3,13 +3,22 @@ if(isset( $_POST['name']))
 $name = $_POST['name'];
 if(isset( $_POST['email']))
 $email = $_POST['email'];
+if(!empty($_POST["send"])) {
+$name = $_POST["userName"];
+$email = $_POST["userEmail"];
 $subject = $_POST["subject"];
-$content = $name["content"];
+$content = $_POST["content"];
+
+$toEmail = "villejonsson80@gmail.com";
+    $mailHeaders = "From: " . $name . "<". $email .">\r\n";
+    if(mail($toEmail, $subject, $content, $mailHeaders)) {
+        $message = "Your contact information is received successfully.";
+        $type = "success";
+    }
+}
+require_once "contact-view.php";
 
 
 
-$recipient = "willejonsson80@gmail.com";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $content, $mailheader) or die("Error!");
-echo "Email sent";
-?>
+
+

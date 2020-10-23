0726826ada411726076a7e8080214e950039760f
wille <willejonsson80@gmail.com>
2020-10-23 09:47:52 +0200

    sC

--- mail.php
+++ mail.php
@@ -3,22 +3,11 @@ if(isset( $_POST['name']))
 $name = $_POST['name'];
 if(isset( $_POST['email']))
 $email = $_POST['email'];
-if(!empty($_POST["send"])) {
-$name = $_POST["userName"];
-$email = $_POST["userEmail"];
-$subject = $_POST["subject"];
-$content = $_POST["content"];
-
-$toEmail = "villejonsson80@gmail.com";
-    $mailHeaders = "From: " . $name . "<". $email .">\r\n";
-    if(mail($toEmail, $subject, $content, $mailHeaders)) {
-        $message = "Your contact information is received successfully.";
-        $type = "success";
-    }
-}
-require_once "contact-view.php";
 
 
-
-
-
+$content="Hello from PHP email script!:";
+$recipient = "willejonsson80@gmail.com";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $content, $mailheader) or die("Error!");
+echo "Email sent!";
+?>

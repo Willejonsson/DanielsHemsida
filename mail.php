b502ec8792db07426aae4c216780657bace32a9b
wille <willejonsson80@gmail.com>
2020-10-26 22:41:12 +0100

    ss

--- mail.php
+++ mail.php
@@ -5,10 +5,10 @@ if(isset( $_POST['email']))
 $email = $_POST['email'];
 if(isset($_POST['Subject']))
 $subject = $_POST['Subject'];
-if(isset( $_content['Content']))
+
+
+
 $content="";
-
-
 $recipient = "willejahbless@gmail.com";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $content,$recipient, $mailheader) or die("Error!");
